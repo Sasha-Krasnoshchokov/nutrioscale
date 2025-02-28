9ee72bdc8bd89058cf7cf7a38e517be59485063c
Oleksandr Krasnoshchokov <krasnoshchokovoleksandr@gmail.com>
2025-02-28 21:55:59 +0200

    configured

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,77 +12,37 @@ const config = {
   theme: {
   	container: {
   		center: true,
-  		padding: '2rem',
   		screens: {
-  			'2xl': '1400px'
+  			'2xl': '1920px',
+  			'xl': '1728px',
+  			'lg': '1440px',
+  			'md': '1024px',
+  			'sm': '820px',
+  			'xs': '420px',
   		}
   	},
   	extend: {
   		colors: {
   			green: {
-  				'500': '#24AE7C',
-  				'600': '#0D2A1F'
-  			},
-  			blue: {
-  				'500': '#79B5EC',
-  				'600': '#152432'
-  			},
-  			red: {
-  				'500': '#F37877',
-  				'600': '#3E1716',
-  				'700': '#F24E43'
+					DEFAULT: '57A04C',
+  				'100': '#DFF1DC',
+  				'200': '#A9C8A2',
+  				'300': '#4AD769',
   			},
   			light: {
-  				'200': '#E8E9E9'
+					DEFAULT: '#FFFFFF',
+  			},
+  			beige: {
+					DEFAULT: '#D6CFC1',
+					'100': '#F0EEEA',
+  			},
+  			gray: {
+					DEFAULT: '#D9D9D9',
   			},
   			dark: {
-  				'200': '#0D0F10',
-  				'300': '#131619',
-  				'400': '#1A1D21',
-  				'500': '#363A3D',
-  				'600': '#76828D',
-  				'700': '#ABB8C4'
+					DEFAULT: '#0E1318',
   			},
   			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
   		},
   		fontFamily: {
   			sans: [
@@ -95,41 +55,42 @@ const config = {
   			pending: 'url(/assets/images/pending-bg.png)',
   			cancelled: 'url(/assets/images/cancelled-bg.png)'
   		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			},
-  			'caret-blink': {
-  				'0%,70%,100%': {
-  					opacity: '1'
-  				},
-  				'20%,50%': {
-  					opacity: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'caret-blink': 'caret-blink 1.25s ease-out infinite'
-  		},
+  		// keyframes: {
+  		// 	'accordion-down': {
+  		// 		from: {
+  		// 			height: '0'
+  		// 		},
+  		// 		to: {
+  		// 			height: 'var(--radix-accordion-content-height)'
+  		// 		}
+  		// 	},
+  		// 	'accordion-up': {
+  		// 		from: {
+  		// 			height: 'var(--radix-accordion-content-height)'
+  		// 		},
+  		// 		to: {
+  		// 			height: '0'
+  		// 		}
+  		// 	},
+  		// 	'caret-blink': {
+  		// 		'0%,70%,100%': {
+  		// 			opacity: '1'
+  		// 		},
+  		// 		'20%,50%': {
+  		// 			opacity: '0'
+  		// 		}
+  		// 	}
+  		// },
+  		// animation: {
+  		// 	'accordion-down': 'accordion-down 0.2s ease-out',
+  		// 	'accordion-up': 'accordion-up 0.2s ease-out',
+  		// 	'caret-blink': 'caret-blink 1.25s ease-out infinite'
+  		// },
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
+  			sm: 'calc(var(--radius) - 4px)',
+  			xs: 'calc(var(--radius) - 6px)'
   		}
   	}
   },
